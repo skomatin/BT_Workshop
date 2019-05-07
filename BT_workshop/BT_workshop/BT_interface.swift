543079b49fd303f1a7203ce2a559c2f0af06c028
skomatin <skomatin@ucsd.edu>
2019-05-06 18:28:35 -0700

    code up and ready

--- BT_workshop/BT_workshop/BT_interface.swift
+++ BT_workshop/BT_workshop/BT_interface.swift
@@ -28,40 +28,36 @@ class BT_Manager: NSObject, CBCentralManagerDelegate, CBPeripheralManagerDelegat
     //This is a required function of the CBCentralManager
     //Called when state of CBCentralanager is updated (when it is initialized)
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if central.state == .poweredOn {
-            print("Starting Scan")
-            startScan()
-        }
-        else {
-            print("Turn on Bluetooth on your phone")
-        }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        //        print(peripheral.name ?? "nil")
-        if  "DSD TECH" == peripheral.name {
-            if self.peripheral == nil {
-                self.peripheral = peripheral
-                self.centralManager.connect(peripheral, options: nil)
-            }
-        }
-    }
-    
-    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        self.centralManager?.stopScan()
-        if nil == self.connectedPeripheral {
-            self.connectedPeripheral = peripheral
-            self.connectedPeripheral.delegate = self        //Allowing the peripheral to discover services
-            print("connected to: \(peripheral.name!)")
-            self.connectedPeripheral.discoverServices(nil)      //look for services for the specified peripheral
-        }
+
+        
+        //Fill in...
+        
         
     }
     
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+
+        
+        //Fill in...
+        
+        
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+
+        
+        //Fill in...
+        
+        
+
+    }
+    
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        print("disconnected from \(self.peripheral.name!)")
-        initVar() //initialize variables to allow for a new connection
-        //Do Something
+
+        
+        //Fill in...
+        
+        
     }
     
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
@@ -72,31 +68,31 @@ class BT_Manager: NSObject, CBCentralManagerDelegate, CBPeripheralManagerDelegat
         print("Listing all available services")
         print(peripheral.services)
         if let services = peripheral.services {
-            for service in services{
-                self.connectedPeripheral.discoverCharacteristics(nil, for: service)
-            }
+
+            
+            //Fill in...
+            
+            
         }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         if let characteristics = service.characteristics {
-            self.serviceDictionary[service] = service.characteristics
-            for characteristic in characteristics{
-                self.connectedPeripheral.setNotifyValue(true, for: characteristic)
-                
-                let data = "a\n".data(using: .utf8)!
-                writeData(data: data, characteristic: characteristic)
-            }
+
+            
+            //Fill in...
+            
+            
         }
     }
     
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        if let data = characteristic.value {
-            notifyObservers(data: data)
-            let val = String.init(data: data, encoding: .utf8) ?? "nil"
-            print("val = ", val)
-        }
-        //print(serviceDictionary)
+
+        
+        //Fill in...
+        
+        
+        
     }
     
     private func notifyObservers(data: Data) {
@@ -107,10 +103,7 @@ class BT_Manager: NSObject, CBCentralManagerDelegate, CBPeripheralManagerDelegat
         connectedPeripheral?.writeValue(data, for: characteristic, type: .withoutResponse)
     }
     
-    private func startScan() {
-        centralManager?.stopScan()
-        centralManager?.scanForPeripherals(withServices: nil, options: nil)
-    }
+//Fill in...
     
     var connectedPeripheral: CBPeripheral!
     
